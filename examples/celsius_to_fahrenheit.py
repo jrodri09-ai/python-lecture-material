e3e6c25b74364cbd162c6293444914a7c5da4503
Jessica Rodriquez <jrodriquez15@cnm.edu>
2023-02-14 12:07:02 -0700

    python first push

---
+++ examples/celsius_to_fahrenheit.py
@@ -0,0 +1,15 @@
+celsius = int(input("Please enter your temp in Celsius."))
+print(celsius)
+
+
+def fahrenheit(c):
+    return 1.8 * c + 32
+
+
+print("The Fahrenheit equivalent of " + str(celsius) + " degrees Celsius is " + str(fahrenheit(celsius)))
+
+# approximation error?
+# round?
+
+
+
