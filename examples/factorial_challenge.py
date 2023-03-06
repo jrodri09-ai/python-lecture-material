c2892707a50334edc5f7407e201b9e2ec33d7eff
Jessica Rodriquez <jrodriquez15@cnm.edu>
2023-03-06 09:34:06 -0700

    factorial challenge compleated

---
+++ examples/factorial_challenge.py
@@ -0,0 +1,27 @@
+# my solution
+import math
+
+n = int(input("Please enter a positive number"))
+
+math.factorial(n)
+
+print("The factorial of the number is " + str(math.factorial(n)))
+
+# suggested solution
+
+
+# def factorial(fac_num):
+    # returned = 1
+
+
+# for item in range(fac_num, 1, -1):
+    # returned *= item
+    # return returned
+
+# print(factorial(3))
+# print(factorial(4))
+# print(factorial(5))
+
+
+
+
