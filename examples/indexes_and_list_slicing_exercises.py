f6b833a94f015022cd281fe52cc09dec94c09538
Jessica Rodriquez <jrodriquez15@cnm.edu>
2023-03-09 11:05:58 -0700

    indexes and list slicing excercises complete.

---
+++ examples/indexes_and_list_slicing_exercises.py
@@ -0,0 +1,16 @@
+list_one = [[0, 2], [4, 6], [8, 10], [12, 14]]
+
+print(list_one[0])
+print(list_one[3][1])
+
+household_items = ["chair", "table", "desk", "lamp", "bed"]
+
+print(household_items[-5])
+
+print("Most people own more than " + str(list_one[0][1]) + " " + household_items[0] + "s.")
+
+list_three = [0.98, 8.76, 6.54, 4.32]
+
+print(list_three[1:])
+print(list_three[1:3])
+print(list_three[0:2])
--- examples/lecture_material.py
+++ examples/lecture_material.py
@@ -608,6 +608,51 @@ not_in_example = 8 not in checked_list  # this returns true, the not in operator
 # value which checked_list contained
 print(not_in_example)
 
+# indexes and list slicing
+
+indexes_example = ["carpet", "hardwood", "linoleum"]
+print(indexes_example[1])
+
+indexes_example2 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# print(indexes_example2[2][0])
+
+# negative indexes : using -1 as the index number when accessing by index gives you the last item of the list
+
+negative = [1, 2, 3, 4, 5]
+print(negative[-1])
+print(negative[-2])
+print(negative[-3])
+print(negative[-4])
+print(negative[-5])
+
+# items that are accessed by index from lists can also be used in expressions like mathmatical expressions and string
+# concatenation, provided that the items being accessed are the correct data type.
+
+mixed = [False, 365, 4.24, "this is the string"]
+print(mixed[2] + 1.76)
+print("I have used \"" + mixed[-1] + "\" as an example too many times.")
+
+# list slicing : if you only wanted to get a specific portion of a list
+
+sliced = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# print(sliced[:4])
+# print(sliced[3:8])
+# print(sliced[6:])
+
+
+# when you do this you get a segment from a larger list
+# You can reassign the items in a list
+
+example = [2, 4, 6, 8, 0]
+print(example)
+example[3] = 10
+example[1:4] = [3, 2, 1]
+print(example)
+example[4:7] = [7, 6, 5]
+print(example)
+
+
+
 
 
 
